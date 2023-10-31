bcc5646f3f449d5fc927665545b203c06b6aae10
Kaidong <kdnie20@gmail.com>
2023-10-31 19:41:36 -0400

    add functionExhibit

---
+++ src/components/atoms/FunctionExhibit.tsx
@@ -0,0 +1,30 @@
+export default function FunctionExhibit({}){
+    let styles = {
+        fill : '#40aea1',
+        fillOpacity:'1',
+        stroke:'#000000',
+        strokeWidth:'0',
+        strokeDasharray:'none'
+    }
+
+    return <div>
+        <svg
+        viewBox="0 0 100 100"
+        id="fn-function">
+            <path
+            style={styles}
+            d="
+            M 0,5 
+            V 45 
+            S 25,40 50,45
+            S 75,50 100,45 
+            V 5
+            S 75,10 50,5
+            S 25,0 0,5
+            z"/>
+
+            
+        </svg>
+    </div> 
+    
+}
