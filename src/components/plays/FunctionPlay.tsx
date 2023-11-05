5129abd960a2ec006aeb79af8c4b45aa29b54a36
Kaidong <kdnie20@gmail.com>
2023-11-05 17:01:10 -0500

    add XY Type

--- src/components/plays/FunctionPlay.tsx
+++ src/components/plays/FunctionPlay.tsx
@@ -3,21 +3,30 @@ import anime, { AnimeParams } from "animejs"
 import FunctionExhibit from "../atoms/FunctionExhibit";
 import Stage from "../atoms/Stage";
 
+const ID_FN1 = 'fn1';
+
 export default function FunctionPlay() {
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
-    let animeParams: AnimeParams = {};
+    const windowSize: XY = [windowWidth, windowHeight];
+    const fn1Size: XY = [100, 100];
 
     useEffect(() => {
         const fnAnimation = anime({
-            targets: '#fn',
+            targets: `#${ID_FN1}`,
             translateX: 250
-        }) 
+        })
     }, []);
 
     
     
     return <Stage windowHeight={windowHeight} windowWidth={windowWidth}>
-        <FunctionExhibit size={[100,100]} offset={[0, windowHeight/2]} exhId="fn" pure={false} />
+        <FunctionExhibit size={fn1Size} offset={getOffsetWithSize(fn1Size, windowSize)} exhId="ID_FN1" pure={false} />
     </Stage>
 }
+
+function getOffsetWithSize(size: XY, window: XY): XY {
+    return [(window[0] - size[0]) / 2, (window[1] - size[1]) / 2];
+}
+
+type XY = [x: number, y: number];
